9778562f6d0843878c28a899fcbc5b4861ae19c5
Yernur <yeastanakulov@one.kz>
2023-04-25 15:00:55 +0600

    initial commit

---
+++ Example/Pods/Target Support Files/ForteExFramework/ForteExFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ForteExFramework : NSObject
+@end
+@implementation PodsDummy_ForteExFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-ForteExFramework_Example/Pods-ForteExFramework_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForteExFramework_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ForteExFramework_Example
+@end
